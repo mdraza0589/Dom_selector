1232b6ffb77f8fc02a859a2542c5e2cdfa346e05
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-01-28 23:18:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let text =  document.querySelector('h2')
+let textInner = text.innerHTML;
+console.log(textInner);
+
+console.log(document.querySelector('input[type=text]'));
+
+const first=document.querySelector('li')
+first.style.backgroundColor="red"
+first.style.padding="10px"
+first.style.fontSize="30px"
+
+
+const list=document.querySelectorAll('li')
+console.log(list[1].style.color="yellow");
